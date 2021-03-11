43ece79764c1e2b281c6ed9ac5f3473a69d6aa3c
Artur-by <Artur.by@mail.ru>
2021-03-11 12:11:49 +0300

    Создал перебор страниц одной категории и записи данных в файл В файле майн создал библиотеку ссылок на страницы товаров категории Смартфоны тест

--- Parsing web.py
+++ Parsing web.py
@@ -4,19 +4,53 @@
 import requests                 # импорт библиотеки Requests
 from lxml import html           # импорт пакета для работы с HTML
 
-url = 'https://sila.by/ntpk/aksessuary_apple'
-response = requests.get(url=url)
-response.encoding = 'cp1251'
-tree = html.fromstring(response.text)
+path_tipe = '//a/span[1]/b[1]'
+path_name = '//a/span[1]/strong[1]'
+path_code = '//div/sup[1]'
+path_price_rub = "//div[@class='price']/div[1]/b[1]"
+path_price_kop = "//div[@class='price']/div[1]/b[2]"
 
+def append_file(name_file, value):  # функция записи в файл
+    file = open(name_file, 'a')
+    file.write(value + '\n')
+    file.close()
 
-path = ["//div/sup[1]", "//a/span[1]/b[1]", "//a/span[1]/strong[1]", "//div[@class='price']/div[1]"]
-# в списке path указаны патчи требуемых позиций
+for i in range(1, 16):              # цикл по перебору страниц мобильных устройств
+    url = f'https://sila.by/ntpk/mobilnye_telefony/page/{i}'
 
-for e in path:                  # цикл по перебору патчей
-    element = tree.xpath(e)
-    q=[]
-    for i in element:
-        #print(i.text)          можно распечатать в столбец элементы
-        q.append(i.text)
-    print(q)
+    response = requests.get(url=url)
+    response.encoding = 'cp1251'
+    tree = html.fromstring(response.text)
+
+    el_tipe = tree.xpath(path_tipe)     # парсим список типов устройств
+    a_tipe = []
+    for i in el_tipe:
+        a_tipe.append(i.text)
+
+    el_name = tree.xpath(path_name)     # парсим название устройств
+    a_name = []
+    for i in el_name:
+        a_name.append(i.text)
+
+    el_code = tree.xpath(path_code)     # парсим коды устройств
+    a_code = []
+    for i in el_code:
+       a_code.append(i.text)
+
+    el_price_rub = tree.xpath(path_price_rub)   # парсим цену устройств-рубли
+    a_price_rub = []
+    for i in el_price_rub:
+        a_price_rub.append(i.text)
+
+    el_price_kop = tree.xpath(path_price_kop)  # парсим цену устройств-копейки
+    a_price_kop = []
+    for i in el_price_kop:
+        a_price_kop.append(i.text)
+
+    item = []                           # новый  список тип+название+код+цена устройства
+    for i in range(len(a_tipe)):
+        item.append(a_tipe[i]+' ' + a_name[i]+' ' + a_code[i] +' '+ a_price_rub[i]+','+a_price_kop[i]+' руб.')
+
+    for i in item:                      # новый список разбиваем построчно и записываем в файл
+        print(i)
+        append_file('mobil.txt', i)
--- main.py
+++ main.py
@@ -1,26 +1,91 @@
-import requests
-from lxml import html
+# Файл по организации меню для выбора парсинга сайта, категорий, товаров
 
-url = 'https://www.21vek.by/bathtubs/'
-response = requests.get(url=url)
+print(" Данная программа позволяет считать  и сохранить данный с заданного сайта ")
+inp_site = input(" Выберите сайт: 1 - sila.by, 2 - 21vek.by ")
+inp_cat = input(" Выберите категорию товаров:\n"
+                " 1- Смартфоны, ноутбуки, компьютеры \n"
+                " 2- Телевизоры, аудио, фото \n"
+                " 3- Бытовая техника \n"
+                " 4- Встраиваемая техника \n"
+                " 5- Посуда и аксессуары \n"
+                " 6- Дом, сад, ремонт авто ")
 
-xpath = '//*[@id="5904035"]/div/span[3]/span[1]'
-tree = html.fromstring(response.text)
-
-for index in range(1, 15):
-    xpath_1 = f'//*[@id="j-result-page-1"]/li[{index}]/dl/div[2]/span[1]/span[2]'
-    element = tree.xpath(xpath_1)
-    text = element[0].text
-    if text == 'р.':
-        xpath_2 = f'//*[@id="j-result-page-1"]/li[{index}]/dl/div[2]/span[1]/span[1]'
-        element = tree.xpath(xpath_2)
-        text = element[0].text
-    print(text)
-
-xpath = "//li/dl[1]/div[@class='catalog-result__item_data' and 1]/dt[@class='result__root' and 1]/a[@class='result__link j-ga_track' and 1]/span[@class='result__name' and 2]"
-element = tree.xpath(xpath)
-a = []
-for e in element:
-    a.append(e.text)
-
-print(a)
+# библиотека ссылок для категории Смартфоны, ноутбуки, компьютеры
+ntpk_aksessuary_apple = 'https://sila.by/ntpk/aksessuary_apple'
+page_aksessuary_apple = 1
+ntpk_aksessuary_mobile = 'https://sila.by/ntpk/aksessuary_mobile'
+page_aksessuary_mobile = 6
+ntpk_aksessuary_dlya_noutbukov = 'https://sila.by/ntpk/aksessuary_dlya_noutbukov'
+page_aksessuary_dlya_noutbukov = 3
+ntpk_aksessuary_k_igrovym_pristavkam = 'https://sila.by/ntpk/aksessuary_k_igrovym_pristavkam'
+page_aksessuary_k_igrovym_pristavkam = 1
+ntpk_web_kamery = 'https://sila.by/ntpk/web-kamery'
+page_web_kamery = 1
+ntpk_vneshnie_akkumulyatory ='https://sila.by/ntpk/vneshnie_akkumulyatory'
+page_vneshnie_akkumulyatory = 4
+ntpk_external_hard_drive ='https://sila.by/ntpk/external_hard_drive'
+page_external_hard_drive = 3
+gaming = 'https://sila.by/gaming'
+page_gaming = 1
+ntpk_gejmpady = 'https://sila.by/ntpk/gejmpady-dzhojstiki-ruli'
+page_gejmpady = 1
+ntpk_graficheskie_planshety = 'https://sila.by/ntpk/graficheskie_planshety'
+page_graficheskie_planshety = 1
+ntpk_zarjadnie_ystroistva = 'https://sila.by/ntpk/zarjadnie_ystroistva'
+page_zarjadnie_ystroistva = 5
+ntpk_zashhitnye_plenki = 'https://sila.by/ntpk/zashhitnye_plenki_stekla'
+page_zashhitnye_plenki = 7
+ntpk_ibp = 'https://sila.by/ntpk/ibp'
+page_ibp = 3
+ntpk_igrovye_kresla = 'https://sila.by/ntpk/igrovye_kresla'
+page_igrovye_kresla = 1
+ntpk_igrovye_pristavki = 'https://sila.by/ntpk/igrovye_pristavki'
+page_igrovye_pristavki = 1
+ntpk_igry_k_pristavkam = 'https://sila.by/ntpk/igry_k_pristavkam'
+page_igry_k_pristavkam = 3
+ntpk_kabel_telefonny = 'https://sila.by/ntpk/kabel_telefonny'
+page_kabel_telefonny = 1
+ntpk_usb_flash = 'https://sila.by/ntpk/usb_flash'
+page_usb_flash = 4
+ntpk_klaviatury = 'https://sila.by/ntpk/klaviatury'
+page_klaviatury = 4
+ntpk_myshi_i_klaviatury = 'https://sila.by/ntpk/myshi_i_klaviatury'
+page_myshi_i_klaviatury = 2
+ntpk_monitory = 'https://sila.by/ntpk/monitory'
+page_monitory = 7
+ntpk_monobloki = 'https://sila.by/ntpk/monobloki'
+page_monobloki = 1
+ntpk_monopody = 'https://sila.by/ntpk/monopody'
+page_monopody = 1
+ntpk_kompyuternye_myshi ='https://sila.by/ntpk/kompyuternye_myshi'
+page_hkompyuternye_myshi = 8
+ntpk_garnitury_i_microfony = 'https://sila.by/ntpk/garnitury_i_microfony'
+page_garnitury_i_microfony = 20
+ntpk_noutbuki = 'https://sila.by/ntpk/noutbuki'
+page_noutbuki = 23
+ntpk_planshety = 'https://sila.by/ntpk/planshety'
+page_planshety = 2
+ntpk_printery_i_mfu = 'https://sila.by/ntpk/printery_i_mfu'
+page_printery_i_mfu = 2
+ntpk_provodnye_telefony = 'https://sila.by/ntpk/provodnye_telefony'
+page_provodnye_telefony = 1
+ntpk_radiotelefony = 'https://sila.by/ntpk/radiotelefony'
+page_radiotelefony = 1
+tpk_rashodnye_materialy = 'https://sila.by/ntpk/rashodnye_materialy'
+page__rashodnye_materialy = 5
+ntpk_setevoe_oborudovanie = 'https://sila.by/ntpk/setevoe_oborudovanie'
+page_setevoe_oborudovanie = 4
+ntpk_sistemnye_bloki = 'https://sila.by/ntpk/sistemnye_bloki'
+page_sistemnye_bloki = 1
+ntpk_mobilnye_telefony = 'https://sila.by/ntpk/mobilnye_telefony'
+page__mobilnye_telefony = 15
+ntpk_sumki_dlya_noutbukov = 'https://sila.by/ntpk/sumki_dlya_noutbukov'
+page_sumki_dlya_noutbukov = 5
+ntpk_smartwatch = 'https://sila.by/ntpk/smartwatch'
+page_smartwatch = 11
+ntpk_chehly = 'https://sila.by/ntpk/chehly'
+page_chehly = 16
+ntpk_chistyashhie_sredstva = 'https://sila.by/ntpk/chistyashhie_sredstva'
+page_chistyashhie_sredstva = 1
+ntpk_elektronnye_knigi = 'https://sila.by/ntpk/elektronnye_knigi'
+page_elektronnye_knigi = 2
